6e1e947ae5b619c205da1d6ffa68e855f1023628
ProfNuru <professornurudeen@gmail.com>
2022-08-25 17:11:17 +0000

    Add files via upload

---
+++ Roadmap.js
@@ -0,0 +1,25 @@
+// Data Types
+
+// Variables
+
+// Core Data Structures
+
+// Control Structures
+
+// Core functions and methods
+
+// Functions: Intro to Functional Programming
+
+// ES6 techniques
+
+// Todolist App
+
+// Shopping Cart
+
+// Snakegame
+
+// OOP
+
+// API Requests
+
+// API Project
---
+++ index.js
@@ -0,0 +1,53 @@
+// Data types and variables
+// numbers (number), strings (str), booleans (bool)
+// null, undefined
+
+// console.log(typeof(10))
+// console.log(typeof("10"))
+// console.log(typeof(true))
+// console.log(undefined)
+
+// Number operations
+    // + - * / %
+// Type Coercion
+
+// console.log(10 + '3')
+
+// String operations
+// console.log('hello' + ' hi')
+
+// String functions and attributes
+// console.log('hello'.length)
+
+// VARIABLES
+// 1. constants
+// You must provide a value when declaring constants
+// You cannot reassign constant variables
+// Does not support hoisting
+const x = 10
+
+// 2. var
+// var y
+// console.log(y)
+// y=30
+// console.log(y)
+
+    // Hoisting
+    // JavaScript brings the scope of var declared
+    // variables to the top
+
+// console.log(z)
+// var z
+
+// 3. let
+// Works similarly to var
+// But does not support hoisting
+// console.log(a)
+// let a
+// console.log(a)
+// a = 200
+// console.log(a)
+
+let mass = 80
+let height = 180
+console.log(height/mass)
